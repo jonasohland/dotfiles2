29aeec70f76e984a35186ec201fd0e1d6df2e432
Jonas Ohland <jonas.ohland@gmail.com>
2024-09-05 10:08:49 +0200

    add lint autocmd for buf projects

--- packages/nvim/lua/ohlano/plugins/lint.lua
+++ packages/nvim/lua/ohlano/plugins/lint.lua
@@ -1,3 +1,8 @@
+local function file_exists(name)
+  local f = io.open(name, "r")
+  return f ~= nil and io.close(f)
+end
+
 local lint = {
   "mfussenegger/nvim-lint",
   config = function()
@@ -5,6 +10,15 @@ local lint = {
       sh = { "shellcheck" },
       proto = { "buf_lint" },
     }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      pattern = { "*.proto" },
+      callback = function()
+        if file_exists("buf.yml") or file_exists("buf.yaml") then
+          require("lint").try_lint()
+        end
+      end,
+    })
   end,
 }
 
--- packages/nvim/lua/ohlano/remap.lua
+++ packages/nvim/lua/ohlano/remap.lua
@@ -9,7 +9,12 @@ local function relativenumbers()
   vim.cmd.set("relativenumber")
 end
 
+local function lint()
+  require("lint").try_lint()
+end
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>kh", vim.cmd.noh)
 vim.keymap.set("n", "<leader>na", numbers)
 vim.keymap.set("n", "<leader>nr", relativenumbers)
+vim.keymap.set("n", "<leader>rl", lint)
