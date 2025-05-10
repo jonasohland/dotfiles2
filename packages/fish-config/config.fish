64e14458db395351b00ceb23b1be4c12e5ab8d33
Jonas Ohland <jonas.ohland@gmail.com>
2025-05-10 09:06:44 +0200

    pwctl volume, vcpkg, functions

--- packages/fish-config/config.fish
+++ packages/fish-config/config.fish
@@ -6,6 +6,7 @@ end
 test -d "$HOME/.local/bin" && fish_add_path -g "$HOME/.local/bin"
 test -d "$HOME/.cargo/bin" && fish_add_path -g "$HOME/.cargo/bin"
 test -d "$HOME/go/bin" && fish_add_path -g "$HOME/go/bin"
+test -d "$HOME/vcpkg" && set -x VCPKG_ROOT "$HOME/vcpkg"
 
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
